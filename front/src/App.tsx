ac90cf630f73a099c4b3f8f8bfa3475c7a66dd59
Alessandro1918 <alessandro1918@hotmail.com>
2023-09-12 16:16:21 -0300

    build: added shadcn/ui lib for component style

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
 
   return (
-    <h1>Hello, world</h1>
+    <>
+      <h1>Hello, world</h1>
+      <Button>Teste</Button>
+    </>
   )
 }
