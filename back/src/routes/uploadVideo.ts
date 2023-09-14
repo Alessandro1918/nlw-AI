25a32b5f0f21a822c991e95e06b311b0e8b6f105
Alessandro1918 <alessandro1918@hotmail.com>
2023-09-13 23:26:46 -0300

    feat: new route: uploadVideo

---
+++ back/src/routes/uploadVideo.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from "fastify"
+import { fastifyMultipart } from "@fastify/multipart"
+import { prisma } from "../lib/prisma"
+import path from "path"
+import { randomUUID } from "crypto"
+import fs from "fs"
+import { pipeline } from "stream"
+import { promisify } from "util"
+
+const pump = promisify(pipeline)
+
+//Upload a mp3 file to the server - POST with a file in the multipart body request (no special field name necessary)
+//(front will get a mp4 file and send it's mp3 here)
+export async function uploadVideoRoute(app: FastifyInstance) {
+
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25    //1MB * 25 = 25MB
+    }
+  })
+
+  app.post("/videos", async (request, reply) => {
+    const data = await request.file()
+
+    if (!data) {
+      return reply.status(400).send({error: "Missing file input"})
+    }
+
+    const fileExtension = path.extname(data.filename)     //".mp3"
+    if (fileExtension !== ".mp3") {
+      return reply.status(400).send({error: "Invalid file input; please upload a MP3"})
+    }
+
+    const fileBaseName = path.basename(data.filename, fileExtension)      //"example.mp3"
+    const newFileName = `${fileBaseName}-${randomUUID()}${fileExtension}` //"example-fdsaf.mp3"
+    const uploadedDestination = path.resolve(__dirname, "../../temp", newFileName)
+
+    //await for the file "data.file" to write it's stream to fs @ destination:
+    await pump(data.file, fs.createWriteStream(uploadedDestination))
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadedDestination
+      }
+    })
+
+    return { video }
+  })
+}
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,14 @@
 import { fastify } from "fastify"
 import { getAllPromptsRoute } from "./routes/getAllPrompts"
+import { uploadVideoRoute } from "./routes/uploadVideo"
 
 const PORT = Number(process.env.PORT) || 4000
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+
 
 app.listen({
   port: PORT
