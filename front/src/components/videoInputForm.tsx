7f8421ccb0b2ce7172656ac525783c1a1658f72a
Alessandro1918 <alessandro1918@hotmail.com>
2023-09-15 17:38:52 -0300

    feat: handleVideoSelection

--- front/src/components/videoInputForm.tsx
+++ front/src/components/videoInputForm.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useMemo, useState } from "react";
 import { FileVideo, Upload } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
@@ -5,20 +6,48 @@ import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 
 export function VideoInputForm() {
+
+  const [ videoFile, setVideoFile ] = useState<File | null>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+    if (!files) {
+      return
+    }
+    setVideoFile(files[0])
+  }
+
+  const previewUrl = useMemo(() => {
+    if (!videoFile) {
+      return null
+    }
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
     <form className="space-y-6">
       <label 
         htmlFor="video" 
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
       >
-        <FileVideo className="w-4 h-4"/>
-        Selecione um video:
+        {
+          previewUrl
+          ? <video src={previewUrl} controls={false} className="pointer-events-none absolute inset-0" />
+          : (
+            <>
+              <FileVideo className="w-4 h-4"/>
+              Selecione um video:
+            </>
+          )
+        }
+        
       </label>
       <input 
         type="file" 
         id="video" 
         accept="video/mp4" 
         className="sr-only"
+        onChange={handleFileSelected}
       />
 
       <Separator />
